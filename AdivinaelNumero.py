7e6172c6461e5ca1cc889c1a253652a93ce896f4
DanielG0m <danicam465@gmail.com>
2024-01-27 15:28:13 -0500

    feat: :sparkles: Adivinando el numero
    Codigo completo, se añadio la opcion de que la computadora adivinara su numero

--- AdivinaelNumero.py
+++ AdivinaelNumero.py
@@ -1,32 +1,44 @@
+import os
 from random import randrange
 n = randrange(101)
-numero=[]
-numero2=[]
 i=1
 titulo="""
 
         Adivina el numero!
 
 """
-print("1. Adivina mi numero\n2. Adivinare tu numero ")
-op=input("Escoge una opcion: ")
-if op == 1:
-    while True:
-        print("Adivine el numero. ")
-        intento= int(input(f"Intento {i}: "))
-        if intento < n:
-            print(f"Es mayor que {intento}")
-        elif intento > n:
-            print(f"Es menor que {intento}")
-        else:
-            print(f"Correcto. Adivinaste en {i} intentos")
-            break
-        i+=1
-elif op == 2:
-    adivinar= input("Ingresa el numero a adivinar: ")
-    numero2.append()
-    while True:
-        print(f"Intento {i}:", randrange(101))
-        print("Ingresa < , >, = ")
-        s=input()
-        i+=1
+opciones=("1. Adivina mi numero\n2. Adivinare tu numero\n3. Salir ")
+while True:
+    print(titulo)
+    print(opciones)
+    op = int(input(">. "))
+
+    if op == 1:
+        while True:
+            print("Adivine el numero. ")
+            intento= int(input(f"Intento {i}: "))
+            if intento < n:
+                print(f"Es mayor que {intento}")
+            elif intento > n:
+                print(f"Es menor que {intento}")
+            else:
+                print(f"Correcto. Adivinaste en {i} intentos")
+                os.system('pause')
+                os.system('cls')
+                break
+            i+=1
+
+    elif op == 2:
+        while True:
+            m=randrange(101)
+            print(f"Intento {i}:",m)
+            print("Ingresa < , >, = ")
+            s=input()
+            if s == "=":
+                print(f"Adivine en {i} intentos B-)")
+                os.system('pause')
+                os.system('cls')
+                break
+            i+=1
+    elif op == 3:
+        break   
