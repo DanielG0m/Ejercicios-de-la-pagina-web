08b549565a69b3f07e74c9dbc7be30044eb1b28f
DanielG0m <danicam465@gmail.com>
2024-01-27 00:54:25 -0500

    refactor: :beers: Correccion de codigo
    Correccion de la aritmetica realizada para verificar los resultados de los dos jugadores.

--- Chachipun.py
+++ Chachipun.py
@@ -4,37 +4,24 @@ contj2=0
 while True:
     primer_jugador=input("Cual fue su jugada jugador 1: ").lower()
     segundo_jugador=input("Cual fue su jugada jugador 2: ").lower()
+    primer_jugador=primer_jugador.replace(' ','')
+    segundo_jugador=segundo_jugador.replace(' ','')
 
-    if primer_jugador == "tijera" and segundo_jugador == "papel":
+    if primer_jugador==segundo_jugador:
+        print("empate")
+        print(f" {contj1} - {contj2}")
+    elif (primer_jugador == "piedra" and segundo_jugador == "tijera") or (primer_jugador == "tijera" and segundo_jugador == "papel") or (primer_jugador == "papel" and segundo_jugador == "piedra"):
         contj1+=1
-        print(f"A: {contj1}  B: {contj2}")
+        print(f" {contj1} - {contj2}")
+    else:
+        contj2=+1
+        print(f" {contj1} - {contj2}")
     
-    if primer_jugador=="tijera" and segundo_jugador=="piedra":
-        contj2+=1
-        print(f"A: {contj1}  B: {contj2}")
-  
-
-    if primer_jugador == "tijera" and segundo_jugador== "tijera":
-        print(f"A: {contj1}  B: {contj2}")
-
-    if primer_jugador == "papel" and segundo_jugador == "tijeras":
-        contj1+=1
-        print(f"A: {contj1}  B: {contj2}")
-    
-    if primer_jugador=="papel" and segundo_jugador=="piedra":
-        contj2+=1
-        print(f"A: {contj1}  B: {contj2}")   
-
-    if primer_jugador == "papel" and segundo_jugador== "papel":
-        print(f"A: {contj1}  B: {contj2}")
-
-    if primer_jugador == "piedra" and segundo_jugador == "papel":
-        contj1+=1
-        print(f"A: {contj1}  B: {contj2}")
-    
-    if primer_jugador=="piedra" and segundo_jugador=="tijera":
-        contj2+=1
-        print(f"A: {contj1}  B: {contj2}")   
-
-    if primer_jugador == "piedra" and segundo_jugador== "piedra":
-        print(f"A: {contj1}  B: {contj2}")
+    if contj1==3:
+        print(f" {contj1} - {contj2}")
+        print("A es el ganador")
+        break
+    elif contj2==3:
+        print(f" {contj1} - {contj2}")
+        print("B es el ganador")
+        break
