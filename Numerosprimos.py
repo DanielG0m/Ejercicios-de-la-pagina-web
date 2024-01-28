754fb636e227ef8e4a493fcb8f7c1e63632315ca
DanielG0m <danicam465@gmail.com>
2024-01-27 19:35:11 -0500

    feat: :fire: Eliminacion
    Eliminacion de la anterior forma de escribir mi codigo, encontrand una forma de rehacer las funciones

--- Numerosprimos.py
+++ Numerosprimos.py
@@ -1,16 +1,27 @@
-def ultimo_digito(x):
-   x=x%10
-   return x
+import math
 
-def verificar_suma(lista_numeros):
-       return sum(lista_numeros)
- 
-m={}
-n=int(input("Ingrese un numero: "))
-m.append(n)
+def es_primo(numero):
+    if numero <=1:
+        return False
+    for i in range(2, int(math.sqrt(numero))+1):
+        if numero % i == 0:
+            return False
+        return True
 
-n=ultimo_digito(n)
-print(n)
+def verificar_priom_o_compuesto():
+   numero= int(input("Ingrese un numero: "))
+   if es_primo(numero):
+      print(numero, "es primo")
+   else:
+       print(numero, "es compuesto")
 
-suma=verificar_suma(m)
-print(suma)
+def primeros_n_primos():
+   n=int(input("Cuantos primos: "))
+   contador=0
+   numero=2
+   while contador < n:
+      if es_primo(numero):
+         print(numero)
+         contador+=1
+      numero += 1
+      
